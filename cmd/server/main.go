69a8fecd17f70781ea8a07212fad5f447f7a58b9
ChiBao <baotochi87@gmail.com>
2025-08-09 11:35:58 +0700

    Add gin

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Starting")
+	r := gin.Default() // gin.Default() is used to create default instance of gin instance
+	/// gin.Context
+	/*
+		gin.Context is a Gin's struct which is designed to encapsulate everything you need during the lifecycle of an HTTP request
+	*/
+	v1 := r.Group("/v1/api")
+	{
+		v1.GET("/ping", Ping)
+	}
+	r.Run()
+}
+
+func Ping(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Ping pong..",
+	})
 }
