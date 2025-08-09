1f794222c87de0e57733448a805b7e9024f186f4
ChiBao <baotochi87@gmail.com>
2025-08-09 11:49:06 +0700

    Add router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,8 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "github.com/BaoTo12/go-ecommerce/internal/routers"
 
 func main() {
-	r := gin.Default() // gin.Default() is used to create default instance of gin instance
-	/// gin.Context
-	/*
-		gin.Context is a Gin's struct which is designed to encapsulate everything you need during the lifecycle of an HTTP request
-	*/
-	v1 := r.Group("/v1/api")
-	{
-		v1.GET("/ping", Ping)
-	}
+	r := routers.NewRouter()
 	r.Run()
-}
-
-func Ping(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Ping pong..",
-	})
 }
