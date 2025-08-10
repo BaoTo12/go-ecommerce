5cbac87c963573f8314ca125a894e422d2e2c719
ChiBao <baotochi87@gmail.com>
2025-08-10 19:29:04 +0700

    build: add mysql driver

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -1,5 +1,16 @@
 package initialize
 
-func InitMysql() {
+import (
+	"fmt"
 
+	"github.com/BaoTo12/go-ecommerce/global"
+	"gorm.io/gorm"
+)
+
+func InitMysql() {
+	mSetting := global.Config.MYSQL
+
+	dsn := "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	string := fmt.Sprintf(dsn, mSetting.Username, mSetting.Password, mSetting.Host, mSetting.Port, mSetting.Dbname)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
